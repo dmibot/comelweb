25df3e7424c06c36e7ae5d161d05dc4e22a32b5a
kerreeehoreee <99607873+dmibot@users.noreply.github.com>
2022-04-19 22:35:35 +0700

    comel

--- commands/tool/inspect.js
+++ commands/tool/inspect.js
@@ -8,11 +8,11 @@ module.exports = {
     desc: "Get Detail Group From Link",
     type: "tool",
     example: "Example : %prefix%command https://chat.whatsapp.com/FpT8DgKeaINLtS1UVwuXMk",
-    start: async(hisoka, m, { text }) => {
+    start: async(comel, m, { text }) => {
         let linkRegex = /chat\.whatsapp\.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i
         let [, code] = text.match(linkRegex) || {}
         if (!code) return m.reply("Link Invalid")
-        let res = await hisoka.getInviteInfo(code)
+        let res = await comel.getInviteInfo(code)
         if (!res) return m.reply(String(res))
         let teks = `
     「 Group Link Inspector 」
@@ -34,15 +34,15 @@ module.exports = {
         `
         let mentions = []
         let id = parseMention(teks)
-        for (let i of id) mentions.push(await hisoka.getContactById(i))
+        for (let i of id) mentions.push(await comel.getContactById(i))
         let pp
         try {
-            pp = await hisoka.getProfilePicUrl(res.id._serialized) || 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
+            pp = await comel.getProfilePicUrl(res.id._serialized) || 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
         } catch {
             pp = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
         }
         let media = await MessageMedia.fromUrl(pp)
-        hisoka.sendMessage(m.from, media, { caption: teks, mentions, quotedMessageId: m.id._serialized })
+        comel.sendMessage(m.from, media, { caption: teks, mentions, quotedMessageId: m.id._serialized })
     },
     isQuery: true
 }
--- commands/tool/searchmsg.js
+++ commands/tool/searchmsg.js
@@ -4,16 +4,16 @@ module.exports = {
     desc: "Search Message From Chat",
     type: "tool",
     example: "Example : %prefix%command <query>, limit",
-    start: async(hisoka, m, { text }) => {
+    start: async(comel, m, { text }) => {
         let [text1, text2] = text.split`,`
-        let fetch = await hisoka.searchMessages(text1, { page: 1, limit: text2 || null, chatId: m.from })
+        let fetch = await comel.searchMessages(text1, { page: 1, limit: text2 || null, chatId: m.from })
         let total = fetch.length
         let sp = total < Number(text2) ? `Hanya Ditemukan ${total} Pesan` : `Ditemukan ${total} pesan`
         m.reply(sp)
 
         fetch.map(async ({ id }) => {
             let { remote: remoteJid, _serialized: serial } = id
-            hisoka.sendMessage(m.from, "Nih Pesannya", { quotedMessageId: serial })
+            comel.sendMessage(m.from, "Nih Pesannya", { quotedMessageId: serial })
         })
     },
     isQuery: true
